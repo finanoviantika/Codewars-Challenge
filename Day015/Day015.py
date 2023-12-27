7786e379ee6b10d0696b7df5b9d2d2ac2efeeb90
unknown <finanoviantika73@gmail.com>
2023-12-27 17:09:03 +0700

    Upload day15 challenge

---
+++ Day015/Day015.py
@@ -0,0 +1,10 @@
+'''
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+'''
+
+def include(arr, item):
+    if item in arr:
+        return True
+    else:
+        return False
